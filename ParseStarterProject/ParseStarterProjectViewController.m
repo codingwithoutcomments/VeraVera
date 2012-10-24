53f39bc95aaad6561d5c39eb97c0c24bf9af3d20
Chris Reath <Chris.Reath@gmail.com>
2012-10-24 08:20:52 -0700

    Added Networking libraries to project.

--- ParseStarterProject/ParseStarterProjectViewController.m
+++ ParseStarterProject/ParseStarterProjectViewController.m
@@ -1,5 +1,7 @@
 #import "ParseStarterProjectViewController.h"
 #import <Parse/Parse.h>
+#import <AFHTTPClient.h>
+#import <AFJSONRequestOperation.h>
 
 @implementation ParseStarterProjectViewController
 
@@ -16,6 +18,18 @@
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
+    
+    NSURL *url = [NSURL URLWithString:@"http://127.0.0.1:8000/api/events/"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    AFJSONRequestOperation *operation = [AFJSONRequestOperation
+                                         JSONRequestOperationWithRequest:request
+                                         success:^(NSURLRequest *request, NSHTTPURLResponse *response, id json) {
+                                             NSLog(@"Events: %@", [json valueForKeyPath:@"events"]);
+                                         } failure:nil];
+    
+    [operation start];
+    
     [super viewDidLoad];
 }
 
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '6.0'
+xcodeproj 'VeraVera.xcodeproj'
+
+pod 'AFNetworking', '1.0'
+pod 'JSONKit'
