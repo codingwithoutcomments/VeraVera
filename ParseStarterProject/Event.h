4b14c4023d266b0ceca70b6f28f9ac58bdc048db
Chris Reath <Chris.Reath@gmail.com>
2012-10-28 12:10:23 -0700

    Events now in Events Array.

---
+++ ParseStarterProject/Event.h
@@ -0,0 +1,13 @@
+//
+//  Event.h
+//  VeraVera
+//
+//  Created by CReath on 10/27/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Event : NSObject
+
+@end
---
+++ ParseStarterProject/Event.m
@@ -0,0 +1,13 @@
+//
+//  Event.m
+//  VeraVera
+//
+//  Created by CReath on 10/27/12.
+//
+//
+
+#import "Event.h"
+
+@implementation Event
+
+@end
